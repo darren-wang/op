a7a22f16748b980b1191656f000e8e70f9815f38
darren-wang <darren_wang@outlook.com>
2016-04-06 22:11:48 +0800

    formatting system and default domain policy

--- oslo_policy/_default_domain.py
+++ oslo_policy/_default_domain.py
@@ -1,126 +1,117 @@
-# This dict is unusable due to following reasons:
-# 1. It's just my expectation to have isolation in this way, the attributes used
-# in this dict are not tested, every operation needs to be tested in the official
-# release to verify if the attributes used here are available.
-# 2. Even the attributes are available, the rules defined here may not be enough
-# to provide desirable domain(tenant) isolation.
+# (DWang) Cloud Service Provider must ensure that all the permissions need
+# to be protected are listed here.
+# If a permission is not found in this dict, it is in the charge of the
+# enforcer and the enforcer will deny requests of that permission by default.
 class DefaultRules(object):
     def __init__(self):
         self.dflt_rules = {
-        # generic
-            # The Syetem developer must ensure that: All the actions need
-            # to be isolated are listed here.
-            # so if an action isn't found in this dict, it is in the charge
-            # of RBAC enforcer.
         'keystone': { 
         # region
-            "list_regions": "role:domain_admin",
-            "get_region": "role:domain_admin",
-            "create_region": "role:domain_admin",
-            "update_region": "role:domain_admin",
-            "delete_region": "role:domain_admin",
+        "list_regions": "role:domain_admin",
+        "get_region": "role:domain_admin",
+        "create_region": "role:domain_admin",
+        "update_region": "role:domain_admin",
+        "delete_region": "role:domain_admin",
         # service
-            "list_services": "role:domain_admin",
-            "get_service": "role:domain_admin",
-            "create_service": "role:domain_admin",
-            "update_service": "role:domain_admin",
-            "delete_service": "role:domain_admin",
+        "list_services": "role:domain_admin",
+        "get_service": "role:domain_admin",
+        "create_service": "role:domain_admin",
+        "update_service": "role:domain_admin",
+        "delete_service": "role:domain_admin",
         # endpoint
-            "get_endpoint": "role:domain_admin",
-            "list_endpoints": "role:domain_admin",
-            "create_endpoint": "role:domain_admin",
-            "update_endpoint": "role:domain_admin",
-            "delete_endpoint": "role:domain_admin",
+        "get_endpoint": "role:domain_admin",
+        "list_endpoints": "role:domain_admin",
+        "create_endpoint": "role:domain_admin",
+        "update_endpoint": "role:domain_admin",
+        "delete_endpoint": "role:domain_admin",
         # domain
-            "list_domains": "role:domain_admin",
-            "create_domain": "role:domain_admin",
-            "delete_domain": "role:domain_admin",
-            "get_domain": "role:domain_admin",
-            "update_domain": "role:domain_admin",
+        "list_domains": "role:domain_admin",
+        "create_domain": "role:domain_admin",
+        "delete_domain": "role:domain_admin",
+        "get_domain": "role:domain_admin",
+        "update_domain": "role:domain_admin",
         # project
-            "list_projects": "role:domain_admin or role:project_admin",
-            "list_user_projects": "role:domain_admin or user_id:%(obj.user.id)s",
-            "create_project": "role:domain_admin",
-            "get_project": "role:domain_admin or role:project_admin",
-            "update_project": "role:domain_admin or role:project_admin",
-            "delete_project": "role:domain_admin",
+        "list_projects": "role:domain_admin or role:project_admin",
+        "list_user_projects": "role:domain_admin or user_id:%(obj.user.id)s",
+        "create_project": "role:domain_admin",
+        "get_project": "role:domain_admin or role:project_admin",
+        "update_project": "role:domain_admin or role:project_admin",
+        "delete_project": "role:domain_admin",
         # user    
-            "list_users": "",
-            "create_user": "role:domain_admin",
-            "get_user": "",
-            "update_user": "role:domain_admin or user_id:%(obj.user.id)s",
-            "delete_user": "role:domain_admin",
-            "change_password": "role:domain_admin or user_id:%(obj.user.id)s",
+        "list_users": "@",
+        "get_user": "@",
+        "create_user": "role:domain_admin",
+        "update_user": "role:domain_admin or user_id:%(obj.user.id)s",
+        "delete_user": "role:domain_admin",
+        "change_password": "role:domain_admin or user_id:%(obj.user.id)s",
         # group
-            "get_group": "role:domain_admin or role:project_admin",
-            "list_groups": "role:domain_admin or role:project_admin",
-            "list_groups_for_user": "role:domain_admin or role:project_admin or user_id:%(obj.user.id)s",
-            "create_group": "role:domain_admin",
-            "update_group": "role:domain_admin",
-            "delete_group": "role:domain_admin",
-            "remove_user_from_group": "role:domain_admin or role:project_admin",
-            "check_user_in_group": "role:domain_admin or role:project_admin",
-            "add_user_to_group": "role:domain_admin or role:project_admin",
-            "list_users_in_group": "role:domain_admin or role:project_admin",
-            "list_projects_for_groups": "",
-            "list_domains_for_groups": "",
+        "get_group": "role:domain_admin or role:project_admin",
+        "list_groups": "role:domain_admin or role:project_admin",
+        "list_groups_for_user": "role:domain_admin or role:project_admin or user_id:%(obj.user.id)s",
+        "create_group": "role:domain_admin",
+        "update_group": "role:domain_admin",
+        "delete_group": "role:domain_admin",
+        "remove_user_from_group": "role:domain_admin or role:project_admin",
+        "check_user_in_group": "role:domain_admin or role:project_admin",
+        "add_user_to_group": "role:domain_admin or role:project_admin",
+        "list_users_in_group": "role:domain_admin or role:project_admin",
+        "list_projects_for_groups": "@",
+        "list_domains_for_groups": "@",
         # grant
-            "check_grant": "role:domain_admin or role:project_admin",
-            "list_grants": "role:domain_admin or role:project_admin",
-            "create_grant": "role:domain_admin or role:project_admin",
-            "revoke_grant": "role:domain_admin or role:project_admin",
+        "check_grant": "role:domain_admin or role:project_admin",
+        "list_grants": "role:domain_admin or role:project_admin",
+        "create_grant": "role:domain_admin or role:project_admin",
+        "revoke_grant": "role:domain_admin or role:project_admin",
         # system grant
-            "check_sys_grant": "@",
-            "list_sys_grants": "@",
-            "create_sys_grant": "@",
-            "revoke_sys_grant": "@",
+        "check_sys_grant": "role:domain_admin",
+        "list_sys_grants": "role:domain_admin",
+        "create_sys_grant": "role:domain_admin",
+        "revoke_sys_grant": "role:domain_admin",
         # role assignment
-            "list_role_assignments": "role:domain_admin or role:project_admin",
+        "list_role_assignments": "role:domain_admin or role:project_admin",
         # role
-            "get_role": "role:domain_admin or role:project_admin",
-            "list_roles": "role:domain_admin or role:project_admin",
-            "create_role": "role:domain_admin",
-            "update_role": "role:domain_admin",
-            "delete_role": "role:domain_admin",
+        "get_role": "role:domain_admin or role:project_admin",
+        "list_roles": "role:domain_admin or role:project_admin",
+        "create_role": "role:domain_admin",
+        "update_role": "role:domain_admin",
+        "delete_role": "role:domain_admin",
         # system role
-            "get_sys_role": "@",
-            "list_sys_roles": "@",
-            "create_sys_role": "@",
-            "update_sys_role": "@",
-            "delete_sys_role": "@",
+        "get_sys_role": "role:domain_admin",
+        "list_sys_roles": "role:domain_admin",
+        "create_sys_role": "role:domain_admin",
+        "update_sys_role": "role:domain_admin",
+        "delete_sys_role": "role:domain_admin",
         # policy
-            "get_policy": "role:domain_admin",
-            "list_policies": "role:domain_admin",
-            "create_policy": "role:domain_admin",
-            "update_policy": "role:domain_admin",
-            "delete_policy": "role:domain_admin",
+        "get_policy": "role:domain_admin",
+        "list_policies": "role:domain_admin",
+        "create_policy": "role:domain_admin",
+        "update_policy": "role:domain_admin",
+        "delete_policy": "role:domain_admin",
         # rule
-            "get_rule": "role:domain_admin",
-            "list_rules": "@",
-            "create_rule": "@",
-            "update_rule": "@",
-            "delete_rule": "@",
+        "get_rule": "role:domain_admin",
+        "list_rules": "role:domain_admin",
+        "create_rule": "role:domain_admin",
+        "update_rule": "role:domain_admin",
+        "delete_rule": "role:domain_admin",
         # token
-            "check_token": "@",
-            "validate_token": "@",
-            "revocation_list": "@",
-            "revoke_token": "@",
+        "check_token": "@",
+        "validate_token": "@",
+        "revocation_list": "@",
+        "revoke_token": "@",
         # other
-            "get_auth_catalog": "@",
-            "get_auth_projects": "@",
-            "get_auth_domains": "@",
-            "list_revoke_events": "@"
-            },
-        # Glance Related
+        "get_auth_catalog": "@",
+        "get_auth_projects": "@",
+        "get_auth_domains": "@",
+        "list_revoke_events": "@"},
+        # Glance related
         'glance': {
-            "context_is_admin": "role:domain_admin",
-            "add_image": "role:project_admin",
-            "delete_image": "role:project_admin",
-            "get_image": "role:project_admin",
-            "get_images": "role:project_admin",
-            "modify_image": "role:project_admin",
-            "publicize_image": "role:project_admin",
-            "download_image": "role:project_admin",
-            "upload_image": "role:project_admin"
-            }
+        "context_is_admin": "role:domain_admin",
+        "add_image": "role:project_admin",
+        "delete_image": "role:project_admin",
+        "get_image": "role:project_admin",
+        "get_images": "role:project_admin",
+        "modify_image": "role:project_admin",
+        "publicize_image": "role:project_admin",
+        "download_image": "role:project_admin",
+        "upload_image": "role:project_admin"}
         }
--- oslo_policy/_system.py
+++ oslo_policy/_system.py
@@ -1,130 +1,122 @@
-# This dict is unusable due to following reasons:
-# 1. It's just my expectation to have isolation in this way, the attributes used
-# in this dict are not tested, every operation needs to be tested in the official
-# release to verify if the attributes used here are available.
-# 2. Even the attributes are available, the rules defined here may not be enough
-# to provide desirable domain(tenant) isolation.
+# (DWang) Cloud Service Provider must ensure that all the permissions need
+# to be protected are listed here.
+# If a permission is not found in this dict, it is in the charge of the
+# enforcer and the enforcer will deny requests of that permission by default.
 class SystemRules(object):
     def __init__(self, conf):
         self.sys_rules = {
-        # generic
-            # The Syetem developer must ensure that: All the actions need
-            # to be isolated are listed here.
-            # so if an action isn't found in this dict, it is in the charge
-            # of RBAC enforcer.
         'keystone': {
-            "csp_domain": ("scope:domain and scope_domain_id:"
-                                            +conf.oslo_policy.CSP_domain_id),
+        "csp_domain": ("scope:domain and scope_domain_id:"
+                                        +conf.oslo_policy.CSP_domain_id),
         # region
-            "list_regions": "rule:csp_domain",
-            "get_region": "rule:csp_domain",
-            "create_region": "rule:csp_domain",
-            "update_region": "rule:csp_domain",
-            "delete_region": "rule:csp_domain",
+        "list_regions": "rule:csp_domain",
+        "get_region": "rule:csp_domain",
+        "create_region": "rule:csp_domain",
+        "update_region": "rule:csp_domain",
+        "delete_region": "rule:csp_domain",
         # service
-            "list_services": "scope:domain",
-            "get_service": "scope:domain",
-            "create_service": "rule:csp_domain",
-            "update_service": "rule:csp_domain",
-            "delete_service": "rule:csp_domain",
+        "list_services": "scope:domain",
+        "get_service": "scope:domain",
+        "create_service": "rule:csp_domain",
+        "update_service": "rule:csp_domain",
+        "delete_service": "rule:csp_domain",
         # endpoint
-            "get_endpoint": "scope:domain",
-            "list_endpoints": "scope:domain",
-            "create_endpoint": "rule:csp_domain",
-            "update_endpoint": "rule:csp_domain",
-            "delete_endpoint": "rule:csp_domain",
+        "get_endpoint": "scope:domain",
+        "list_endpoints": "scope:domain",
+        "create_endpoint": "rule:csp_domain",
+        "update_endpoint": "rule:csp_domain",
+        "delete_endpoint": "rule:csp_domain",
         # domain
-            "list_domains": "rule:csp_domain",
-            "create_domain": "rule:csp_domain",
-            "delete_domain": "rule:csp_domain",
-            "get_domain": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.domain.id)s)",
-            "update_domain": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.domain.id)s)",
+        "list_domains": "rule:csp_domain",
+        "create_domain": "rule:csp_domain",
+        "delete_domain": "rule:csp_domain",
+        "get_domain": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.domain.id)s)",
+        "update_domain": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.domain.id)s)",
         # project
-            "list_projects": "rule:csp_domain or scope_domain_id:%(qStr.domain_id)s",
-            "list_user_projects": "rule:csp_domain or scope_domain_id:%(qStr.domain_id)s",
-            "create_project": "rule:csp_domain or (scope:domain and scope_domain_id:%(reqBody.project.domain_id)s)",
-            "get_project": "rule:csp_domain or scope_domain_id:%(obj.project.domain_id)s",
-            "update_project": "rule:csp_domain or ((scope:domain and scope_domain_id:%(obj.project.domain_id)s) or scope_project_id:%(obj.project.id)s)",
-            "delete_project": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.project.domain_id)s)",
+        "list_projects": "rule:csp_domain or scope_domain_id:%(qStr.domain_id)s",
+        "list_user_projects": "rule:csp_domain or scope_domain_id:%(qStr.domain_id)s",
+        "create_project": "rule:csp_domain or (scope:domain and scope_domain_id:%(reqBody.project.domain_id)s)",
+        "get_project": "rule:csp_domain or scope_domain_id:%(obj.project.domain_id)s",
+        "update_project": "rule:csp_domain or ((scope:domain and scope_domain_id:%(obj.project.domain_id)s) or scope_project_id:%(obj.project.id)s)",
+        "delete_project": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.project.domain_id)s)",
         # user    
-            "list_users": "rule:csp_domain or scope_domain_id:%(qStr.domain_id)s",
-            "create_user": "rule:csp_domain or (scope:domain and scope_domain_id:%(reqBody.user.domain_id)s)",
-            "get_user": "rule:csp_domain or scope_domain_id:%(obj.user.domain_id)s",
-            "update_user": "rule:csp_domain or (user_id:%(obj.user.id)s or (scope:domain and scope_domain_id:%(obj.user.domain_id)s))",
-            "delete_user": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.user.domain_id)s)",
-            "change_password": "rule:csp_domain or (user_id:%(obj.user.id)s or (scope:domain and scope_domain_id:%(obj.user.domain_id)s))",
+        "list_users": "rule:csp_domain or scope_domain_id:%(qStr.domain_id)s",
+        "create_user": "rule:csp_domain or (scope:domain and scope_domain_id:%(reqBody.user.domain_id)s)",
+        "get_user": "rule:csp_domain or scope_domain_id:%(obj.user.domain_id)s",
+        "update_user": "rule:csp_domain or (user_id:%(obj.user.id)s or (scope:domain and scope_domain_id:%(obj.user.domain_id)s))",
+        "delete_user": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.user.domain_id)s)",
+        "change_password": "rule:csp_domain or (user_id:%(obj.user.id)s or (scope:domain and scope_domain_id:%(obj.user.domain_id)s))",
         # group
-            "get_group": "rule:csp_domain or scope_domain_id:%(obj.group.domain_id)s",
-            "list_groups": "rule:csp_domain or scope_domain_id:%(qStr.domain_id)s",
-            "list_groups_for_user": "rule:csp_domain or scope_domain_id:%(obj.user.domain_id)s",
-            "create_group": "rule:csp_domain or (scope:domain and scope_domain_id:%(reqBody.group.domain_id)s)",
-            "update_group": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.group.domain_id)s)",
-            "delete_group": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.group.domain_id)s)",
-            "remove_user_from_group": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.group.domain_id)s)",
-            "check_user_in_group": "rule:csp_domain or scope_domain_id:%(obj.group.domain_id)s",
-            "add_user_to_group": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.group.domain_id)s)",
-            "list_users_in_group": "rule:csp_domain or scope_domain_id:%(obj.group.domain_id)s",
-            "list_projects_for_groups": "",
-            "list_domains_for_groups": "",
+        "get_group": "rule:csp_domain or scope_domain_id:%(obj.group.domain_id)s",
+        "list_groups": "rule:csp_domain or scope_domain_id:%(qStr.domain_id)s",
+        "list_groups_for_user": "rule:csp_domain or scope_domain_id:%(obj.user.domain_id)s",
+        "create_group": "rule:csp_domain or (scope:domain and scope_domain_id:%(reqBody.group.domain_id)s)",
+        "update_group": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.group.domain_id)s)",
+        "delete_group": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.group.domain_id)s)",
+        "remove_user_from_group": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.group.domain_id)s)",
+        "check_user_in_group": "rule:csp_domain or scope_domain_id:%(obj.group.domain_id)s",
+        "add_user_to_group": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.group.domain_id)s)",
+        "list_users_in_group": "rule:csp_domain or scope_domain_id:%(obj.group.domain_id)s",
+        "list_projects_for_groups": "",
+        "list_domains_for_groups": "",
         # grant
-            "check_grant": "rule:csp_domain or ((scope_project_id:%(url.project_id)s) or (scope:domain and (scope_domain_id:%(obj.domain.id)s or scope_domain_id:%(obj.project.domain_id)s)))",
-            "list_grants": "rule:csp_domain or ((scope_project_id:%(url.project_id)s) or (scope:domain and (scope_domain_id:%(obj.domain.id)s or scope_domain_id:%(obj.project.domain_id)s)))",
-            "create_grant": "rule:csp_domain or ((scope_project_id:%(url.project_id)s) or (scope:domain and (scope_domain_id:%(obj.domain.id)s or scope_domain_id:%(obj.project.domain_id)s)))",
-            "revoke_grant": "rule:csp_domain or ((scope_project_id:%(url.project_id)s) or (scope:domain and (scope_domain_id:%(obj.domain.id)s or scope_domain_id:%(obj.project.domain_id)s)))",
+        "check_grant": "rule:csp_domain or ((scope_project_id:%(url.project_id)s) or (scope:domain and (scope_domain_id:%(obj.domain.id)s or scope_domain_id:%(obj.project.domain_id)s)))",
+        "list_grants": "rule:csp_domain or ((scope_project_id:%(url.project_id)s) or (scope:domain and (scope_domain_id:%(obj.domain.id)s or scope_domain_id:%(obj.project.domain_id)s)))",
+        "create_grant": "rule:csp_domain or ((scope_project_id:%(url.project_id)s) or (scope:domain and (scope_domain_id:%(obj.domain.id)s or scope_domain_id:%(obj.project.domain_id)s)))",
+        "revoke_grant": "rule:csp_domain or ((scope_project_id:%(url.project_id)s) or (scope:domain and (scope_domain_id:%(obj.domain.id)s or scope_domain_id:%(obj.project.domain_id)s)))",
         # system grant 
-            "check_sys_grant": "@",
-            "list_sys_grants": "@",
-            "create_sys_grant": "@",
-            "revoke_sys_grant": "@",
+        "check_sys_grant": "rule:csp_domain",
+        "list_sys_grants": "rule:csp_domain",
+        "create_sys_grant": "rule:csp_domain",
+        "revoke_sys_grant": "rule:csp_domain",
         # role assignment
-            "list_role_assignments": "rule:csp_domain or (user_id:%(user.id)s or scope_domain_id:%(qStr.domain_id)s or scope_project_id:%(qStr.project_id)s)",
+        "list_role_assignments": "rule:csp_domain or (user_id:%(user.id)s or scope_domain_id:%(qStr.domain_id)s or scope_project_id:%(qStr.project_id)s)",
         # role
-            "get_role": "rule:csp_domain or scope_domain_id:%(obj.role.domain_id)s",
-            "list_roles": "rule:csp_domain or scope_domain_id:%(qStr.domain_id)s",
-            "create_role": "rule:csp_domain or (scope:domain and scope_domain_id:%(reqBody.role.domain_id)s)",
-            "update_role": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.role.domain_id)s)",
-            "delete_role": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.role.domain_id)s)",
+        "get_role": "rule:csp_domain or scope_domain_id:%(obj.role.domain_id)s",
+        "list_roles": "rule:csp_domain or scope_domain_id:%(qStr.domain_id)s",
+        "create_role": "rule:csp_domain or (scope:domain and scope_domain_id:%(reqBody.role.domain_id)s)",
+        "update_role": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.role.domain_id)s)",
+        "delete_role": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.role.domain_id)s)",
         # system role
-            "get_sys_role": "@",
-            "list_sys_roles": "@",
-            "create_sys_role": "@",
-            "update_sys_role": "@",
-            "delete_sys_role": "@",
+        "get_sys_role": "rule:csp_domain",
+        "list_sys_roles": "rule:csp_domain",
+        "create_sys_role": "rule:csp_domain",
+        "update_sys_role": "rule:csp_domain",
+        "delete_sys_role": "rule:csp_domain",
         # policy
-            "get_policy": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.policy.domain_id)s)",
-            "list_policies": "rule:csp_domain or (scope:domain and scope_domain_id:%(qStr.domain_id)s)",
-            "create_policy": "rule:csp_domain or (scope:domain and scope_domain_id:%(reqBody.policy.domain_id)s)",
-            "update_policy": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.policy.domain_id)s)",
-            "delete_policy": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.policy.domain_id)s)",
+        "get_policy": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.policy.domain_id)s)",
+        "list_policies": "rule:csp_domain or (scope:domain and scope_domain_id:%(qStr.domain_id)s)",
+        "create_policy": "rule:csp_domain or (scope:domain and scope_domain_id:%(reqBody.policy.domain_id)s)",
+        "update_policy": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.policy.domain_id)s)",
+        "delete_policy": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.policy.domain_id)s)",
         # rule
-            "get_rule": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.policy.domain_id)s)",
-            "list_rules": "@",
-            "create_rule": "@",
-            "update_rule": "@",
-            "delete_rule": "@",
+        "get_rule": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.policy.domain_id)s)",
+        "list_rules": "rule:csp_domain or (scope:domain and scope_domain_id:%(qStr.domain_id)s)",
+        "create_rule": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.policy.domain_id)s)",
+        "update_rule": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.policy.domain_id)s)",
+        "delete_rule": "rule:csp_domain or (scope:domain and scope_domain_id:%(obj.policy.domain_id)s)",
         # token
-            "check_token": "@",
-            "validate_token": "@",
-            "revocation_list": "@",
-            "revoke_token": "@",
+        "check_token": "@",
+        "validate_token": "@",
+        "revocation_list": "@",
+        "revoke_token": "@",
         # other
-            "get_auth_catalog": "@",
-            "get_auth_projects": "@",
-            "get_auth_domains": "@",
-            "list_revoke_events": "@"
-            },
+        "get_auth_catalog": "@",
+        "get_auth_projects": "@",
+        "get_auth_domains": "@",
+        "list_revoke_events": "@"
+        },
         # Glance Related
         'glance': {
-            "csp_domain": ("scope:domain and scope_domain_id:"
-                                            +conf.oslo_policy.CSP_domain_id),
-            "context_is_admin": "rule:csp_domain",
-            "add_image": "scope:project",
-            "delete_image": "@",
-            "get_image": "@",
-            "get_images": "@",
-            "modify_image": "@",
-            "publicize_image": "@",
-            "download_image": "@",
-            "upload_image": "@"
-            }
+        "csp_domain": ("scope:domain and scope_domain_id:"
+                                        +conf.oslo_policy.CSP_domain_id),
+        "context_is_admin": "rule:csp_domain",
+        "add_image": "scope:project",
+        "delete_image": "@",
+        "get_image": "@",
+        "get_images": "@",
+        "modify_image": "@",
+        "publicize_image": "@",
+        "download_image": "@",
+        "upload_image": "@"}
         }
